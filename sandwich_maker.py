eec7457f46ab8e7ecf84b0819039527e4fbdc42e
DrRojo <christopher.d.berns@gmail.com>
2024-06-16 11:26:22 -0400

    build: defining check_resources

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -5,7 +5,8 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources.get(item,0):
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        #####
